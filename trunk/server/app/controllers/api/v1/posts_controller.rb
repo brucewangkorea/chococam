f7c03b2cb6fea16242703f82dcadff2ba7cc7dbd
c2dm@chocopepper.com <c2dm@chocopepper.com@9d711877-4931-d6a8-cdc5-f21aca903fb2>
2012-11-23 09:16:07 +0000

    bug fix for delete comment

--- trunk/server/app/controllers/api/v1/posts_controller.rb
+++ trunk/server/app/controllers/api/v1/posts_controller.rb
@@ -255,7 +255,7 @@ class Api::V1::PostsController < ApplicationController
     if @comment.delete
 			unless @post.nil?
 				# 2012-10-11 brucewang
-				@post.comment_count+=1
+				@post.comment_count-=1
 				@post.save
 			end
 			
